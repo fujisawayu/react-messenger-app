ee00174e686bd069feb713bfc50629900dafcb92
fujisawa <fujisawa5523@gmail.com>
2023-07-29 23:05:42 +0900

    Add sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.scss';
+import Sidebar from './components/Sidebar';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* {sidebar} */}
+      <Sidebar />
+
+      {/* {hoge} */}
+    </div>
+  );
 }
 
 export default App;
