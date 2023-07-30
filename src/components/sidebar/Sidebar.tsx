916fe5632a867a436632ae5f29ae2296c5035fb0
fujisawa <fujisawa5523@gmail.com>
2023-07-30 20:55:14 +0900

    Add sidebarHeader component and search field

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './Sidebar.scss';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* sidebarLeft */}
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
+      {/* sidebarRight */}
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Messenger</h3>
+          <ExpandMoreIcon />
+        </div>
+
+        {/* sidebarChannels */}
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>チャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" alt="" />
+              <div className="accountName">
+                <h4>アカウント名</h4>
+                <span>アカウントID</span>
+              </div>
+            </div>
+
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
