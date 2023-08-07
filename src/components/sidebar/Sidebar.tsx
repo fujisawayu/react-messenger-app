7c5f07d7e8e1ebfd005965940a207574202fe172
fujisawa <fujisawa5523@gmail.com>
2023-08-07 22:30:01 +0900

    Add input processing from addDoc and prompt

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,10 +9,20 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import { auth, db } from '../../firebase';
 import { useAppSelector } from '../../app/hooks';
 import useCollection from '../../hooks/useCollection';
+import { addDoc, collection } from 'firebase/firestore';
 
 const Sidebar = () => {
-  const user = useAppSelector((state) => state.user);
+  const user = useAppSelector((state) => state.user.user);
   const { documents: channels } = useCollection('channels');
+  const addChannel = async () => {
+    let channelName: string | null = prompt('新しいチャンネルを作成します');
+
+    if (channelName) {
+      await addDoc(collection(db, 'channels'), {
+        channelName: channelName,
+      });
+    }
+  };
 
   return (
     <div className="sidebar">
@@ -39,7 +49,7 @@ const Sidebar = () => {
               <ExpandMoreIcon />
               <h4>チャンネル</h4>
             </div>
-            <AddIcon className="sidebarAddIcon" />
+            <AddIcon className="sidebarAddIcon" onClick={() => addChannel()} />
           </div>
 
           <div className="sidebarChannelList">
